f8579e7cc189bd8e1bf765727d78b0670c02bd95
Petr <petr.grigorev@sparm.com>
2021-01-12 14:38:54 +0300

    fix selected className

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -8,9 +8,9 @@ const Filter = ( { setFilter, filters }) => {
   const selected = 'filter-button-selected';
   return (
     <div className="filters">
-      <button className={cx('filter-button', {selected: filters === 'SHOW_ALL'})} onClick={() => { setFilter('SHOW_ALL') } }>Показать все</button>
-      <button className={cx('filter-button', {selected: filters === 'SHOW_COMPLETED'})} onClick={() => { setFilter('SHOW_COMPLETED') } }>Показать готовые</button>
-      <button className={cx('filter-button', {selected: filters === 'SHOW_UNCOMPLETED'})} onClick={() => { setFilter('SHOW_UNCOMPLETED') } }>Показать не готовые</button>
+      <button className={cx('filter-button', {[selected]: filters === 'SHOW_ALL'})} onClick={() => { setFilter('SHOW_ALL') } }>Показать все</button>
+      <button className={cx('filter-button', {[selected]: filters === 'SHOW_COMPLETED'})} onClick={() => { setFilter('SHOW_COMPLETED') } }>Показать готовые</button>
+      <button className={cx('filter-button', {[selected]: filters === 'SHOW_UNCOMPLETED'})} onClick={() => { setFilter('SHOW_UNCOMPLETED') } }>Показать не готовые</button>
     </div>
   )
 }
