0aba904eff22f278fb92760c478b2181d4d37ea1
Petr <petr.grigorev@sparm.com>
2021-01-12 12:07:39 +0300

    added classnames

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import classNames from 'classnames';
+import styles from '../../App.css';
+
+let cx = classNames.bind(styles);
 
 const Filter = ( { setFilter, filters }) => {
-  
+  const selected = 'filter-button-selected';
   return (
     <div className="filters">
-      <button className={`filter-button ${filters === 'SHOW_ALL' ? 'filter-button-selected' : ''}`} onClick={() => { setFilter('SHOW_ALL') } }>Показать все</button>
-      <button className={`filter-button ${filters === 'SHOW_COMPLETED' ? 'filter-button-selected' : ''}`} onClick={() => { setFilter('SHOW_COMPLETED') } }>Показать готовые</button>
-      <button className={`filter-button ${filters === 'SHOW_UNCOMPLETED' ? 'filter-button-selected' : ''}`} onClick={() => { setFilter('SHOW_UNCOMPLETED') } }>Показать не готовые</button>
+      <button className={cx('filter-button', {selected: filters === 'SHOW_ALL'})} onClick={() => { setFilter('SHOW_ALL') } }>Показать все</button>
+      <button className={cx('filter-button', {selected: filters === 'SHOW_COMPLETED'})} onClick={() => { setFilter('SHOW_COMPLETED') } }>Показать готовые</button>
+      <button className={cx('filter-button', {selected: filters === 'SHOW_UNCOMPLETED'})} onClick={() => { setFilter('SHOW_UNCOMPLETED') } }>Показать не готовые</button>
     </div>
   )
 }
