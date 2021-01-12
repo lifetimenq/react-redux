0695708be715f0eec8ea6e2c187a6c0c2862a6e4
Petr <petr.grigorev@sparm.com>
2021-01-12 12:17:09 +0300

    fixed statistics

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,10 +1,5 @@
 
-const Statistics = ({chapters, subChapters}) => {
-
-  const allChapters = chapters.length + subChapters.length
-  const completeChapters = chapters.filter(el => el.completed).length + subChapters.filter(el => el.completed).length
-  const percent = Math.floor((completeChapters / allChapters) * 100)
-
+const Statistics = ({ ownProps : {allChapters, percent}}) => {
   return (
     <div className="statistics">
       <div >
--- src/Components/Statistics/index.js
+++ src/Components/Statistics/index.js
@@ -3,7 +3,13 @@ import { connect } from 'react-redux';
 import Statistics from './Statistics';
 
 const mapStateToProps = (state) => {
-  return state.content;
+  const allChapters = state.content.chapters.length + state.content.subChapters.length
+  const completeChapters = state.content.chapters.filter(el => el.completed).length + state.content.subChapters.filter(el => el.completed).length
+  const percent = Math.floor((completeChapters / allChapters) * 100)
+  return { ownProps: {
+    allChapters,
+    percent
+  }};
 };
 
 export default connect(mapStateToProps)(Statistics)
